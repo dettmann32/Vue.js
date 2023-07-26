7b5274769fec6eee65c3c6ffe90acba0c5f95117
Deathman <lucasdettmann8@gmail.com>
2023-07-26 17:46:51 -0300

    commit

---
+++ Estudos01/app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+   <TheHeader class="header" v-show="showHeader"></TheHeader>
+    
+<div v-show="nome">
+  Nome: {{ name }}
+  Sobrenome: {{ lastname }}
+
+</div>
+<div v-if="access == 'ADMIN'"> admin </div>
+<div v-else-if="access == 'marketing'"> marketing </div>
+<div v-else> user </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+   TheHeader
+  },
+  data(){
+    return{
+      showHeader: true,
+      nome : true,
+      name : 'Jon snow ',
+      lastname: 'Snow',
+      access: 'admin'.toUpperCase()
+ 
+ } 
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ Estudos01/app/src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>  
+        Header
+    </header>
+      
+</template>
+
+<script>
+    export default{
+
+    }
+
+</script>
+
+<style>
+.header{
+    background-color:gray;
+    font-size: 32px;
+    color: white;  
+    text-align: center;
+}
+div{
+    text-align: center;
+    font-size: 25px;
+}
+
+</style>
