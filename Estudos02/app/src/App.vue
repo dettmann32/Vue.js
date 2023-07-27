f75af5faaa22c54600f35f944986ecbccb112aa1
Deathman <lucasdettmann8@gmail.com>
2023-07-27 18:56:54 -0300

    commit

---
+++ Estudos02/app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="grid grid-cols-4 text-center">
+    <div v-for=" (c,index) in caixa" v-bind:key="index" class="box1">
+      <p>{{c}}</p>
+    </div>
+    <div v-for="c in caixa2" v-bind:key="c.id" class="box1">
+      {{c.language}}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+    
+  },
+  data(){
+    return{
+      caixa : ['java','python','css'],
+      caixa2 : [
+        {id:1, language:'php'},
+        {id:2, language:'html'},
+        {id:3, language:'kotlin'},
+        {id:4, language:'kotlin'},
+        {id:5, language:'kotlin'}
+  
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
