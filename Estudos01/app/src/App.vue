34d6989d584717efbf3cbf38ac148c54d79fbd4a
Deathman <lucasdettmann8@gmail.com>
2023-07-27 17:49:48 -0300

    commit

--- Estudos01/app/src/App.vue
+++ Estudos01/app/src/App.vue
@@ -1,36 +1,28 @@
 <template>
-   <TheHeader class="header" v-show="showHeader"></TheHeader>
-    
-<div v-show="nome">
-  Nome: {{ name }}
-  Sobrenome: {{ lastname }}
-
-</div>
-<div v-if="access == 'ADMIN'"> admin </div>
-<div v-else-if="access == 'marketing'"> marketing </div>
-<div v-else> user </div>
+   <div>
+     <TheHeader class="header"></TheHeader>
+     <TheMainContainer></TheMainContainer>
+     
+   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
+import TheMainContainer from './components/TheMainContainer.vue'
 
 
 export default {
   name: 'App',
   components: {
-   TheHeader
+   TheHeader,TheMainContainer,
   },
   data(){
     return{
-      showHeader: true,
-      nome : true,
-      name : 'Jon snow ',
-      lastname: 'Snow',
-      access: 'admin'.toUpperCase()
- 
+
+     }
  } 
 }
-}
+
 </script>
 
 <style>
---
+++ Estudos01/app/src/components/TheMainContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam non repellat consequatur magnam corporis ratione natus, aperiam odit delectus iure similique unde facilis quo sapiente fugit suscipit architecto ipsam quibusdam.
+        </p>
+    </main>
+</template>
+
+<script>
+    export default {
+        name :'TheMainContainer'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
