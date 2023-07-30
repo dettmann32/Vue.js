60ad6f0fbbbc320dfd14aa3049377114b6c49716
Deathman <lucasdettmann8@gmail.com>
2023-07-29 22:12:26 -0300

    commit

---
+++ Estudo07/app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TheHeader></TheHeader>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+  export default{
+    name : 'App',
+   components:{
+      TheHeader
+    }
+  
+  }
+    
+  
+
+</script>
---
+++ Estudo07/app/src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>Email</h1>
+        <p v-if="mostrar_email">{{ email }}</p>
+        <button @click="showEmail">{{ mostrar }}</button>
+    </header>
+</template>
+
+<script>
+    export default{
+        name : 'TheHeader',
+    
+    data(){
+        return{
+            mostrar : 'Mostrar Email',
+            mostrar_email : false,
+            email : "joão@gmail.com"
+            }
+        },
+    methods: {
+        showEmail(){
+            this.mostrar_email = !this.mostrar_email
+            if (this.mostrar_email){
+                this.mostrar = 'Esconder Email'
+            }else{
+                this.mostrar = 'Mostrar Email'
+            }
+        }
+
+        }
+    }
+
+
+</script>
