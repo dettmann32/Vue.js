7cdb63fe42c61711511977edf873adbc32190157
Deathman <lucasdettmann8@gmail.com>
2023-08-01 19:52:39 -0300

    commit

---
+++ Estudo08/app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TheHeader></TheHeader>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue"
+export default {
+  components:{
+    TheHeader,
+  },
+  setup () {
+      return {
+        
+      }
+  }
+}
+</script>
+
+<style  scoped>
+
+</style>
---
+++ Estudo08/app/src/components/TheHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <form action="" >
+            <input type="txt" v-model="email1">
+            <button @click="enviar($event)">{{ texto }}</button>
+            <p v-if="variavel">{{ email }}</p>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        
+
+        return {
+            texto : 'Aperte para esconder',
+            variavel : false,
+            email : 'joão@email.com'
+        }
+    },
+    methods:{
+        enviar(e){  
+            e.preventDefault()          
+            this.variavel = !this.variavel
+            if(this.variavel){this.texto = 'Aperte para mostrar'
+        }else{
+            this.texto = 'Aperte para esconder'
+        }
+        console.log(this.email1)
+
+        }
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
