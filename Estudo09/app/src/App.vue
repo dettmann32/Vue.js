99dbc14a753b5e9843d31ebe034ee1c2da546533
Deathman <lucasdettmann8@gmail.com>
2023-08-12 15:06:15 -0300

    commit

--- Estudo09/app/src/App.vue
+++ Estudo09/app/src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  
+  <TeSte></TeSte>
   <router-view/>
 </template>
+<script>
+import TeSte from './components/TeSte.vue';
+ export default{
 
+  components:{
+    TeSte
+  }
+}
+</script>
 <style>
 
 
---
+++ Estudo09/app/src/components/TeSte.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+       <form action="https://google.com"
+       @submit.prevent="onSubmit">
+        <input 
+        type="text"
+        v-model="name"
+        @keyup.enter="on()">
+       </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TeSte",
+    data () {
+        
+
+        return {
+            name:''
+        }
+    },
+    methods:{
+        on(){
+            console.log(this.name)
+        }
+    }
+}
+</script>
+
+<style  scoped>
+
+</style>
