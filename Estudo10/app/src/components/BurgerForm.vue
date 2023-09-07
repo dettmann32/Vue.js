e7adf4a781b8a2bf2c336212eba2c72b38c63d18
Deathman <lucasdettmann8@gmail.com>
2023-09-07 19:00:21 -0300

    commit

--- Estudo10/app/src/components/BurgerForm.vue
+++ Estudo10/app/src/components/BurgerForm.vue
@@ -1,10 +1,26 @@
 <template>
-    <div class="mt-10 flex justify-center flex-col">
-        <h2>Faça seu pedido</h2>
+    <div class="mt-10 flex justify-center flex-col h-[400px]">
+        <h1 class="text-center mb-20 -mt-20 text-5xl">Faça seu pedido</h1>
+        <div class="my-5 text-4xl border-l-4 border-l-yellow-300 pl-3">
+            <h2>Pão</h2>
+        </div>
         <div>
-            <select name="Pão" id="">
-                <option value="">Selecione um Pão</option>
+            <select name="Pão" id="" class="w-[40vw] text-center  border-solid border-black border-2">
+                <option v-for="pao in paes" :key="pao.id" value="paor">{{ pao.tipo }}</option>
             </select>
+        </div>
+        <div class="my-5 text-4xl border-l-4 border-l-yellow-300 pl-3">
+            <h2>Carnes</h2>
+        </div>
+        <div>
+            <select name="Carnes" id="" class="w-[40vw] text-center  border-solid border-black border-2">
+                <option v-for="carne in carnes" :key="carne.id" value="carner">{{ carne.tipo }}</option>
+            </select>
+        </div>
+        <div>
+            <button @click="createdBurger()">
+                Crie seu Burger
+            </button>
         </div>
     </div>
 </template>
@@ -12,6 +28,37 @@
 <script>
 
 export default{
-    name: 'BurgerForm'
+    name: 'BurgerForm',
+
+    data(){
+        return{
+            paes:null,
+            carnes:null,
+        }
+    },
+    methods:{
+        async getElementes(){
+            const req = await fetch("http://localhost:3000/ingredientes")
+            const data = await req.json()
+            this.paes = data.paes
+            this.carnes = data.carnes
+            
+
+        },
+        async createdBurger(e){
+            e.preventDefault()
+            const data : {
+                
+            }
+
+        }
+    },
+    mounted(){
+        this.getElementes()
+    }
+        
+
+
+    
 }
 </script>
--- Estudo10/app/src/views/HomeView.vue
+++ Estudo10/app/src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <main>
        <section  class="flex items-center h-[600px] bg-left bg-cover bg-no-repeat" style="background-image: url('https://www.sabornamesa.com.br/media/k2/items/cache/b9ad772005653afce4d4bd46c2efe842_XL.jpg');" >
        <div>
-        <h1 class="text-yellow-200 text-4xl sm:text-6xl md:text-8xl">Make Your Burger</h1>
+        <h1 class="text-yellow-200 text-4xl sm:text-6xl md:text-8xl bg-gray-700 bg-opacity-50 p-5">Make Your Burger</h1>
        </div>
        </section>
        <section>
