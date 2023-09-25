adb20e55f9cefbf81cddd3d39cd7caf32191a5bb
Deathman <lucasdettmann8@gmail.com>
2023-09-25 18:40:59 -0300

    commit

---
+++ Estudo11/tela_de_login/src/views/AboutView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-center">
+    <div class="container">
+      <form class="form">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="border-gray-600 border ">
+        <label for="password" >password</label>
+        <input type="password" name="password">
+
+
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name:'AboutView',
+
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.form{
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+}
+
+.input{
+  border:black;
+  background-color: gray;
+
+  
+}
+</style>
---
+++ Estudo11/tela_de_login/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
