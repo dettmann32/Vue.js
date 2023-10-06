3172b124be08960e97b1075f8c9e7ebdab3ce1e0
Deathman <lucasdettmann8@gmail.com>
2023-10-06 20:01:24 -0300

    cadastro

--- Estudo11/tela_de_login/src/views/HomeView.vue
+++ Estudo11/tela_de_login/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex justify-center">
     <div class="container">
-      <form class="form" @submit.prevent="created">
+      <form class="form" @submit.prevent="created()">
 
         <label for="name">Nome</label>
-        <input v-model="form.user" type="text" >
+        <input v-model="user" type="text" >
 
         <label for="email">Email</label>
-        <input type="email" name="email" class="border-gray-600 border " v-model="form.email">
+        <input type="email" name="email" class="border-gray-600 border " v-model="email">
 
 
         <label for="password" >password</label>
-        <input type="password" name="password" v-model="form.password">
+        <input type="password" name="password" v-model="password">
 
         <div class="btn">
           <button type="submit">
@@ -33,18 +33,27 @@ export default {
   name: 'HomeView',
   data(){
     return{
-      form:{
+      
       user:null,
       email:null,
       password:null,
-      }
+      
 
     }
   },
   methods:{
     async created(){
-     const data = await axios.post('http://localhost:3333/user/user',this.form)
-     console.log(data.data)
+      const response = await axios.post('http://localhost:3333/user/login',{
+      user:this.user,
+      email:this.email,
+      password:this.password
+     }).then()
+
+     this.user = null
+     this.email = null
+     this.password = null
+    
+     if(response.)
       
     }
   }
